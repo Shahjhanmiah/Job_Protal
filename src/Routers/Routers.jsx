034e4884de4d51ce5ddd4b69bbbbbdd9d9a979b6
mdshahjhan <mdshahjha478877@gmail.com>
2024-01-15 11:02:44 +0600

    fouad

---
+++ src/Routers/Routers.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Main";
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children:[
+  
+        {
+          path:'/',
+          element:<Home></Home>
+          
+        },
+       
+
+
+  
+      ]
+    },
+  ]);
+export default router  
